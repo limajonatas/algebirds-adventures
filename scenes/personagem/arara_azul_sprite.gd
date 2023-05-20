002e35de640059aaf052e81729e43de097c25b49
Jonatas <44263115+limajonatas@users.noreply.github.com>
2023-05-20 19:25:59 -0300

    :sparkles: rotacionar e mover pássaro
    rotacionar e mover pássaro de acordo com o direção do canhão.

--- scenes/personagem/arara_azul_sprite.gd
+++ scenes/personagem/arara_azul_sprite.gd
@@ -1,37 +1,62 @@
 extends Sprite2D
-var cont := 0;
+var cont := 0
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		var key_event := event as InputEventKey
 		
 		if key_event.is_action_pressed("ui_down"):
 			if cont > -3:
-				cont -= 1
-				var rotation_angle = deg_to_rad(10) # Define o ângulo de rotação (15 graus)
-				var translation_offset := Vector2();
-				if cont+1 == 0:
-					translation_offset = Vector2(6, 5) # Define o deslocamento de posição (10 unidades em x e y)
-				elif cont+1 == -1:
-					translation_offset = Vector2(5, 10)
-				elif cont+1 == -2:
-					rotation_angle = deg_to_rad(11)
-					translation_offset = Vector2(4, 15)
+				#var rotation_angle = deg_to_rad(8) # Define o ângulo de rotação (8 graus)
+				
+				
+				# Define a rotação do sprite
+				#self.rotation += rotation_angle
+				
+				# Define a nova posição do sprite
+				if cont == 3:
+					self.rotation = deg_to_rad(-13)
+					self.position = Vector2(-10, -7)
+				elif cont == 2:
+					self.rotation = deg_to_rad(-2)
+					self.position = Vector2(-4, -4)
+				elif cont == 1:
+					self.rotation = deg_to_rad(0)
+					self.position = Vector2(0, 0)
+				elif cont == 0:
+					self.rotation = deg_to_rad(6)
+					self.position = Vector2(4, 4)
+				elif cont == -1:
+					self.rotation = deg_to_rad(13)
+					self.position = Vector2(8, 15)
+				elif cont == -2:
+					self.rotation = deg_to_rad(26)
+					self.position = Vector2(13, 30)
 					
-				# Define a rotação do sprite
-				self.rotation += rotation_angle
-				
-				# Define a nova posição do sprite
-				self.position += translation_offset
-				
+				cont -= 1	
 		elif key_event.is_action_pressed("ui_up"):
-			#transforma o sprite rotaciona um pouco para esquerda (abaixo de 0 grau) e mudar um pouco a posicao para cima e para esquerda"
-			if cont > 3:
-				cont += 1
-				var rotation_angle = deg_to_rad(-15) # Define o ângulo de rotação (-15 graus)
-				var translation_offset = Vector2(-10, -10) # Define o deslocamento de posição (-10 unidades em x e y)
-				
+			if cont < 3:
 				# Define a rotação do sprite
-				self.rotation += rotation_angle
+				#self.rotation += rotation_angle
 				
-				# Define a nova posição do sprite
-				self.position += translation_offset
+				if cont == -3:
+					self.rotation = deg_to_rad(13)
+					self.position = Vector2(8, 15) 
+					
+				elif cont == -2:
+					self.rotation = deg_to_rad(6)
+					self.position = Vector2(4, 4)
+				elif cont == -1:
+					self.rotation = deg_to_rad(0)
+					self.position = Vector2(0, 0)
+				elif cont == 0:
+					self.rotation = deg_to_rad(-2)
+					self.position = Vector2(-4, -4)
+				elif cont == 1:
+					self.rotation = deg_to_rad(-13)
+					self.position = Vector2(-10, -7)
+				elif cont == 2:
+					self.rotation = deg_to_rad(-22)
+					self.position = Vector2(-17, -10)
+					
+				cont += 1
