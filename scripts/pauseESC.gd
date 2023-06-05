e030a4695522befbf2fe132e9e98a76ca8d310f6
Jonatas <jonataslimafsa@gmail.com>
2023-06-05 11:49:13 -0300

    botão de pausar funcional

---
+++ scripts/pauseESC.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var canva: CanvasLayer = $CanvasLayer;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	canva.visible=false
+	get_tree().paused = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if Input.is_action_just_pressed('pause'):
+		if get_tree().paused == false:
+			get_tree().paused = true
+			canva.visible=true
+		else:
+			get_tree().paused = false
+			canva.visible=false
+
+func _on_voltar_voltar():
+	print('clicou')
+	get_tree().paused = false
+	canva.visible=false
+
+func _on_pausa_pausar():
+	print('clicou')
+	get_tree().paused = true
+	canva.visible=true
--- scripts/pauseJogo.gd
+++ scripts/pauseJogo.gd
@@ -1,11 +1,11 @@
 extends Sprite2D
 
-@onready var controlerPause: CanvasLayer = $CanvasLayer
+signal  pausar
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		if get_rect().has_point(get_local_mouse_position()):
 			if event.is_pressed():
-				print('clicou')
-				
+				#get_tree().paused = true;
+				emit_signal("pausar")
 			else:
-				print('else')         
+				print('else')
