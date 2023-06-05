0a7f92fb1e1661a041399c2265d391b2e0b82494
WesleiSantos <weslei200912@gmail.com>
2023-06-05 20:55:16 -0300

    :hammer: added music global

--- scripts/Main_menu.gd
+++ scripts/Main_menu.gd
@@ -1,7 +1,13 @@
 extends Control
 
+var button_play: Button
+var button_options: Button
+
 func _ready():
 	get_node("Button_play").pressed.connect(_on_Button_pressed)
+
 func _on_Button_pressed():
-	print("veio")
 	get_node("/root/Global").goto_scene("res://scenes/menu_selection.tscn")
+
+
+
--- scripts/Menu_selection.gd
+++ scripts/Menu_selection.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	get_node("Button").pressed.connect(_on_Button_pressed)
+	get_node("Button_back").pressed.connect(_on_Button_pressed)
 func _on_Button_pressed():
 	print("veio")
 	get_node("/root/Global").goto_scene("res://scenes/main_menu.tscn")
